134b19d9be2e538d4ff97778a9a13f15001e32b6
kagaleraj <134764142+kagaleraj@users.noreply.github.com>
2023-11-29 15:01:48 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FristProjectPipline
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a, b, c;
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("Addition=" +c);
+        }
+        public void Sub()
+        {
+            int a, b, c;
+            a = 50;
+            b = 20;
+            c = a - b;
+            Console.WriteLine("Sub=" + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();   
+            p.Add();
+            p.Sub();
+            Console.ReadLine();
+        }
+    }
+}
